023cf3beeee08e8f86cf3739afe86cc89f33dffe
Bernd Troßmann <114260977+ben1070XXL@users.noreply.github.com>
2023-11-20 21:58:29 +0100

    Show user data in EditAddressComponent

--- src/app/dialog-edit-address/dialog-edit-address.component.ts
+++ src/app/dialog-edit-address/dialog-edit-address.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { User } from 'src/models/user.class';
 
 @Component({
   selector: 'app-dialog-edit-address',
   templateUrl: './dialog-edit-address.component.html',
-  styleUrls: ['./dialog-edit-address.component.scss']
+  styleUrls: ['./dialog-edit-address.component.scss'],
 })
 export class DialogEditAddressComponent {
+  user: User = new User();
+  loading = false;
+
+  constructor(public dialogRef: MatDialogRef<DialogEditAddressComponent>){}
+
+  saveUser() {
+    
+  }
 
 }
