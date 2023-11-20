8048dc27063f35db8121f83fb478ac7f9dfcc577
Bernd Troßmann <114260977+ben1070XXL@users.noreply.github.com>
2023-11-20 18:11:12 +0100

    Show All Users in the table

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,21 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from 'src/models/user.class';
+import { Firestore, collection, onSnapshot } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss'],
 })
-export class UserComponent {
+export class UserComponent implements OnInit, OnDestroy {
   user = new User();
+  allUsers: any = [];
 
-  constructor(public dialog: MatDialog) {
-    
+  firestore: Firestore = inject(Firestore);
+  unsubUsers: any;
+
+  constructor(public dialog: MatDialog) {}
+
+  ngOnInit(): void {
+    this.unsubUsers = this.subUsersList();
+  }
+
+  subUsersList() {
+    return onSnapshot(this.getUsersRef(), (users) => {
+      console.log('Received changes from DB: ', users);
+      this.allUsers = [];
+
+      users.forEach((user) => {
+        this.allUsers.push(user.data());
+      })
+
+      console.log('Current Users: ', this.allUsers);
+    });
   }
 
   openDialog() {
     this.dialog.open(DialogAddUserComponent);
   }
+
+  getUsersRef() {
+    return collection(this.firestore, 'users');
+  }
+
+  ngOnDestroy(): void {
+    this.unsubUsers();
+  }
+
+
 }
